302443e461307f7684c6f62a9e5f0c5617a7555a
saif786a <alamsaif86@gmail.com>
2024-03-27 15:29:29 +0530

    Initial commit

---
+++ src/main/java/com/mailSender/springbootsendconformationlink/SpringBootSendConformationLinkApplication.java
@@ -0,0 +1,14 @@
+package com.mailSender.springbootsendconformationlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSendConformationLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSendConformationLinkApplication.class, args);
+		System.out.println("hi everyone");
+	}
+
+}
