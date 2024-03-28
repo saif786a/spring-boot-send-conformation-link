f61ab24f60516e3256858f67de123b1871afdbdf
saif786a <alamsaif86@gmail.com>
2024-03-29 01:08:56 +0530

    Complete Entity Class

--- src/main/java/com/mailSender/springbootsendconformationlink/Entity/Confirmationtoken.java
+++ src/main/java/com/mailSender/springbootsendconformationlink/Entity/Confirmationtoken.java
@@ -1,4 +1,9 @@
 package com.mailSender.springbootsendconformationlink.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="")
 public class Confirmationtoken {
 }
